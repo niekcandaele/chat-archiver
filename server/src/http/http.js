27c598a39a3521d06afd702b220ed0e259d02e73
Niek Candaele <niek@candaele.dev>
2022-03-13 22:43:09 +0100

    fix: route any unknown urls to index.html to fix React routing on refresh

--- server/src/http/http.js
+++ server/src/http/http.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import rateLimit from 'express-rate-limit';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 import { config } from '../config.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { searchRouter } from './routes/search.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export async function getHttp() {
   return new Promise((resolve, reject) => {
@@ -20,6 +25,10 @@ export async function getHttp() {
     app.use('/search', searchRouter)
 
     app.use(errorHandler)
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname + '/../../public/index.html'));
+    });
 
     app.listen(config.get('http.port'), () => {
       console.log('HTTP server listening on port ' + config.get('http.port'));
